b0e666ccda4fd2d5faf5d85eada991767311a8fc
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-06 19:17:00 +0330

    feat: add clock label and alarm time input fields (hour, minute)

--- Alarm_clock.py
+++ Alarm_clock.py
@@ -19,6 +19,20 @@ y = int((screen_height / 2) - (window_height / 2))
 # Apply window size and position
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+#lable show clock
+label_clock_now = ttk.Label(root, text="12:30:30", font=("Times New Roman", 20))
+label_clock_now.place(relx=0.44, rely=0.05, anchor="w")
 
+#entey and lable hour
+entry_hour = ttk.Entry(root, width=20)
+entry_hour.place(relx=0.5, rely=0.45, anchor="center")
+label_hour = ttk.Label(root, text="hour", font=("Times New Roman", 20))
+label_hour.place(relx=0.1, rely=0.45, anchor="w")
+
+#entry and lable minute
+entry_minute = ttk.Entry(root, width=20)
+entry_minute.place(relx=0.5, rely=0.55, anchor="center")
+label_minute = ttk.Label(root, text="minute", font=("Times New Roman", 20))
+label_minute.place(relx=0.1, rely=0.55, anchor="w")
 
 root.mainloop()
